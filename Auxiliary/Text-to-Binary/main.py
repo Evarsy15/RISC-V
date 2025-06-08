8082d346e854324036e9e3db17c1d1bf57d107e4
Evarsy15 <rabbitnix@postech.ac.kr>
2025-06-09 00:50:54 +0900

    [Update] `main.py` of RISC-V Text-to-Binary Converter

--- Auxiliary/Text-to-Binary/main.py
+++ Auxiliary/Text-to-Binary/main.py
@@ -13,7 +13,20 @@ prog_args.add_argument('-o', '--output', type = str, default = '',
                        help = "Output text file")
 
 if __name__ == '__main__':
-    riscv_txt = open(prog_args.input, 'r');
-    riscv_insts = Parse_RISCV_Text(riscv_txt.readlines())
+    input_name = prog_args.input
+    input_file = open(input_name, 'r')
+    riscv_text = input_file.readlines()
+    
+    riscv_insts = Parse_RISCV_Text(riscv_text)
     riscv_bin = Convert_RISCV_Inst(riscv_insts)
     
+    output_name = prog_args.output
+    if not output_name:
+        i = input_name.find('.')
+        prefix, suffix = input_name[:i], input_name[i+1:]
+        output_name = prefix + '_out' + suffix
+    output_file = open(output_name, 'w')
+    output_file.writelines(riscv_bin)
+
+
+
